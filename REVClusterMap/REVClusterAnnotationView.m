963bf27a186efa4979c2c99b21843b89bbefc10f
Genki Mine <gomfucius@gmail.com>
2012-07-31 06:41:23 -0700

    Adjust size

--- REVClusterMap/REVClusterAnnotationView.m
+++ REVClusterMap/REVClusterAnnotationView.m
@@ -20,11 +20,11 @@
     self = [super initWithAnnotation:annotation reuseIdentifier:reuseIdentifier];
     if ( self )
     {
-        label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 26, 26)];
+        label = [[UILabel alloc] initWithFrame:CGRectMake(0, -2, 33, 33)];
         [self addSubview:label];
         label.textColor = [UIColor whiteColor];
         label.backgroundColor = [UIColor clearColor];
-        label.font = [UIFont boldSystemFontOfSize:11]; 
+        label.font = [UIFont boldSystemFontOfSize:12];
         label.textAlignment = UITextAlignmentCenter;
         label.shadowColor = [UIColor colorWithRed:0.f green:0.f blue:0.f alpha:0.75f];
         label.shadowOffset = CGSizeMake(-0.5f, -0.5f);
